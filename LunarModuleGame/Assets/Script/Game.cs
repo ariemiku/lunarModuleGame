528b9169ea0fa72260642e2fbf753167682bd39d
ariemiku <arie@alfasystem.co.jp>
2015-05-11 10:50:49 +0900

    ゲームの流れを作る

---
+++ LunarModuleGame/Assets/Script/Game.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+enum eStatus{
+	ePlay,
+	eGameOver,
+};
+
+public class Game : MonoBehaviour {
+	private eStatus m_Status;
+
+	public Text gameoverText;
+	
+	// Use this for initialization
+	void Start () {
+		m_Status = eStatus.ePlay;
+		Transit (m_Status);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (m_Status) {
+		case eStatus.ePlay:
+			UpdatePlay();
+			break;
+		case eStatus.eGameOver:
+			UpdateGameover();
+			break;
+		}
+	}
+
+	// シーンを代える
+	void Transit(eStatus NextStatus){
+		switch (NextStatus) {
+		case eStatus.ePlay:
+			StartPlay(m_Status);
+			break;
+		case eStatus.eGameOver:
+			StartGameover(m_Status);
+			break;
+		}
+		m_Status = NextStatus;
+	}
+
+
+	// play状態の開始関数
+	void StartPlay(eStatus PrevStatus){
+		// 代わった時に1回しかやらないことをする
+		gameoverText = GameObject.Find ("Canvas/TextGameover").GetComponent<Text> ();
+		gameoverText.text = "";
+
+	}
+
+	// Game状態の開始関数
+	void StartGameover(eStatus PrevStatus){
+		// 代わった時に1回しかやらないことをする
+		gameoverText.text = "GAMEOVER\npush space";
+	}
+
+	// Play状態の更新関数
+	void UpdatePlay(){
+		// RightShiftキーでゲームオーバーに切り替える（仮）
+		if(Input.GetKeyDown(KeyCode.RightShift))
+		{
+			Transit (eStatus.eGameOver);
+		}
+
+	}
+
+	// Game状態の更新関数
+	void UpdateGameover(){
+		// spaceキーでTitleに切り替える
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("Title");
+		}
+	}
+
+
+
+}
---
+++ LunarModuleGame/Assets/Script/Title.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// spaceキーでシーンを切り替える
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("LunarModuleGame");
+		}
+	
+	}
+}
